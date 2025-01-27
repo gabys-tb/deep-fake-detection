8b75f85daafdad296fe9b8da335d5068411248bd
Raphael Aroldo Carreiro Mendes - PrestServ <fjqv@gr13b05n05.petrobras.biz>
2025-01-27 14:56:57 -0300

    evaluate + metrics

--- experiments/utils/model_utils.py
+++ experiments/utils/model_utils.py
@@ -40,9 +40,78 @@ def train_function(model, train_dataloader, n_epochs, criterion, optimizer, devi
         plt.savefig(loss_plot_name)
         plt.close()
 
-#TO DO
-def evaluate_function():
-    pass
+def get_metrics(output, label):
+    # Flatten the 3D arrays to 1D for metric calculation
+    output_flat = output.flatten()
+    label_flat = label.flatten()
+
+    # Ensure that the output and label are binary arrays (0s and 1s)
+    output_flat = (output_flat > 0).astype(int)
+    label_flat = (label_flat > 0).astype(int)
+
+    # Calculate metrics
+    p = precision_score(label_flat, output_flat, zero_division=1)
+    r = recall_score(label_flat, output_flat, zero_division=1)
+    f1 = f1_score(label_flat, output_flat, zero_division=1)
+    a = accuracy_score(label_flat, output_flat)
+
+    return p, r, f1, a
+
+def evaluate(model, device, criterion, dataloader=None):
+    """
+    Evaluate the model on a list of cube names and their corresponding targets.
+
+    Parameters:
+    - model: The PyTorch model to evaluate.
+    - device: The device (CPU/GPU) to run the model on.
+    - get_metrics: Function to calculate the precision, recall, F1, and accuracy for each example.
+    - dataloader: To load the data/label.
+
+    Returns:
+    - avg_p, avg_r, avg_f1, avg_a: Averaged Precision, Recall, F1-score, and Accuracy metrics.
+    """
+    model.eval()  # Set the model to evaluation mode
+    all_results = []  # List to store tuples (seismic, label, prediction)
+
+    total_p, total_r, total_f1, total_a = 0, 0, 0, 0  # Initialize metrics
+
+    total_loss = 0
+    
+    with torch.no_grad():
+        for img, label in dataloader:
+            # Convert sis and label to tensors and move to device
+            img = img.to(device)
+            label = label.to(device)
+
+            # Get model prediction
+            output = model(img)
+            loss = criterion(prediction = output, target = label)
+            total_loss += loss.item()
+
+            # Convert outputs to numpy for evaluation and plotting
+            output = output.squeeze().cpu().numpy()
+            label = label.squeeze().cpu().numpy()
+
+            threshold = 0.5
+            output = output > threshold
+
+            # Calculate metrics for this example
+            p, r, f1, a = data_utils.get_metrics(output, label)
+            
+            # Sum up the metrics for averaging later
+            total_p += p
+            total_r += r
+            total_f1 += f1
+            total_a += a
+    
+    # Average the metrics across all samples
+    avg_p = total_p / num_samples
+    avg_r = total_r / num_samples
+    avg_f1 = total_f1 / num_samples
+    avg_a = total_a / num_samples
+    avg_loss = total_loss / num_samples
+
+    return avg_p, avg_r, avg_f1, avg_a, avg_loss
 
 def active_learning(list_1, list_2, batch_size, n_epochs, model, optimizer, data_path, num_of_rounds=100, criterion = nn.BCEWithLogitsLoss()):
     datalist_1 = list_1
